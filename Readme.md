6f3f099b14a2593412edf2640aa24ae0323ccab4
xmyhhh <xmycissy@outlook.com>
2022-04-04 00:14:43 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,5 +1,35 @@
-## ���
- һ��c++Ƕ��Mono����ʱ����C#��¶�ķ�����demo�������о�Unity����Ľű�����
+## 简介
 
-## ˵��
-����ʱ����һ�´��룬�����Ŀ�ʹ��뻹��Ҫ������һ�� ,�п��������ĵ�
+一个 c++嵌入 Mono 运行时调用 C#暴露的方法的 demo，用于研究 Unity 引擎的脚本机制
+
+## 依赖
+
+- Mono [下载地址](http://www.mono-project.com/download/)
+
+>注意，我已经把 Mono 的 x64 运行库文件拷贝到了`\dependency\mono` 目录下，因此仅编译C++代码可以不用安装 Mono，但要编译C#代码到 dll 文件任然需要安装 Mono
+
+## 编译指南
+### C++编译生成exe需要配置如下：
+
+* Compile include:
+`\dependency\mono\include\mono-2.0`
+
+* Linker lib path:
+`\dependency\mono\lib`
+
+* Linker lib:
+`mono-2.0-sgen.lib`
+
+### C#编译生成dll (On Windows)
+* 安装Mono以后，把`\Mono\bin`文件夹添加到环境变量的PATH中
+> 64位安装完成后将在：C:\Program Files\Mono\bin。32位在C:\Program Files (x86)\Mono\bin
+* 在cmd中进入到存放Program.cs文件的目录
+  
+* mcs Program.cs -t:library
+## 运行指南
+直接运行大概率会报找不到dll的错误
+
+
+## 说明
+
+仅暂时保存一下代码，依赖的库和代码还需要再整理一下 ,有空再完善文档
---
+++ Simple-Demo/main.cpp
@@ -0,0 +1,112 @@
+﻿#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/class.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/mono-config.h>
+#include <string>
+#include <Windows.h>// 用于运行时加载DLL库
+struct Component
+{
+	int id;
+	int tag;
+};
+
+Component* Components;
+uint32_t num_Components;
+MonoClassField* native_handle_field;
+MonoDomain* domain;
+MonoClass* Component_class;
+//获取属性
+int ManagedLibrary_Component_get_id_Internal(const Component* component)
+{
+	return component->id;
+}
+
+int ManagedLibrary_Component_get_tag(MonoObject* this_ptr)
+{
+	Component* component;
+	mono_field_get_value(this_ptr, native_handle_field, reinterpret_cast<void*>(&component));
+	return component->tag;
+}
+//获取组件
+MonoArray* ManagedLibrary_Component_GetComponents()
+{
+	MonoArray* array = mono_array_new(domain, Component_class, num_Components);
+
+	for (uint32_t i = 0; i < num_Components; ++i)
+	{
+		MonoObject* obj = mono_object_new(domain, Component_class);
+		mono_runtime_object_init(obj);
+		void* native_handle_value = &Components[i];
+		mono_field_set_value(obj, native_handle_field, &native_handle_value);
+		mono_array_set(array, MonoObject*, i, obj);
+	}
+
+	return array;
+}
+
+int main(int argc, const char* argv[])
+{
+
+	
+	// 设置搜索路径，用于搜索mscorlib.dll
+	std::string MonoPath = "../dependency/mono";
+
+	std::string monoLibPath = MonoPath + "/lib";
+	std::string monoEtcPath = MonoPath + "/etc";
+	//// 运行时加载mono-2.0-sgen.dll库
+	//HMODULE module = LoadLibrary((monoLibPath + "/mono-2.0-sgen.dll").c_str());//因为设置成了“使用Unicode字符集”,所以前面要加L
+	//if (module == NULL)
+	//{
+	//	printf("加载mono-2.0-sgen.dll动态库失败\n");
+	//	return 0;
+	//}
+	mono_set_dirs(monoLibPath.c_str(), monoEtcPath.c_str());
+
+
+
+	mono_config_parse(NULL);
+
+	//获取应用域 
+	const char* managed_binary_path = "MainTest.dll";
+	domain = mono_jit_init(managed_binary_path);
+
+	//加载程序集MainTest.dll
+	MonoAssembly* assembly = mono_domain_assembly_open(domain, managed_binary_path);
+	MonoImage* image = mono_assembly_get_image(assembly);
+
+	// 注册函数，这里是以后主要添加的地方 ，所有的函数都是通过该方法绑定的
+	mono_add_internal_call("ManagedLibrary.Component::get_id_Internal", reinterpret_cast<void*>(ManagedLibrary_Component_get_id_Internal));
+	mono_add_internal_call("ManagedLibrary.Component::get_Tag", reinterpret_cast<void*>(ManagedLibrary_Component_get_tag));
+	mono_add_internal_call("ManagedLibrary.Component::GetComponents", reinterpret_cast<void*>(ManagedLibrary_Component_GetComponents));
+	Component_class = mono_class_from_name(image, "ManagedLibrary", "Component");
+	native_handle_field = mono_class_get_field_from_name(Component_class, "native_handle");
+
+	num_Components = 5;
+	Components = new Component[5];
+	for (uint32_t i = 0; i < num_Components; ++i)
+	{
+		Components[i].id = i;
+		Components[i].tag = i * 4;
+	}
+
+	MonoClass* main_class = mono_class_from_name(image, "ManagedLibrary", "Main");
+
+	const bool include_namespace = true;
+	MonoMethodDesc* managed_method_desc = mono_method_desc_new("ManagedLibrary.Main:TestComponent()", include_namespace);
+	MonoMethod* managed_method = mono_method_desc_search_in_class(managed_method_desc, main_class);
+	mono_method_desc_free(managed_method_desc);
+
+	mono_runtime_invoke(managed_method, NULL, NULL, NULL);
+
+	//清理mono环境
+	mono_jit_cleanup(domain);
+
+	delete[] Components;
+
+
+	while (true) {
+		
+	}
+	return 0;
+}
